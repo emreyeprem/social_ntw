faa46eaae5ad06c3c0b57d63a04ebd08d9463088
Emre Akurek <emreyeprem@gmail.com>
2019-09-26 12:32:25 -0500

    redux integrated

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,14 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 const App = () => (
-    <Router>
+    <Provider store={store}>
+      <Router>
         <Fragment>
           <Navbar />
            <Route exact path='/' component={Landing} />
@@ -19,6 +24,7 @@ const App = () => (
            </section>
         </Fragment>
      </Router>
+   </Provider>
 );
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import axios from 'axios';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -12,11 +14,38 @@ const Register = () => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+    const onSubmit = async e => {
+        e.preventDefault();
+        if(password !== password2) {
+            console.log('Password do not match');
+        }else {
+            console.log('Success')
+            // const newUser = {
+            //     name,
+            //     email,
+            //     password
+            // }
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
+            //     const body = JSON.stringify(newUser);
+            //     const res = await axios.post('/api/users', body, config);
+            //     console.log(res.data);
+            // } catch(err) {
+            //     console.error(err.response.data);
+            // }
+        }
+    }
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-             <form className="form" action="create-profile.html">
+             <form className="form"
+                   onSubmit={e => onSubmit(e)}>
               <div className="form-group">
                <input type="text" placeholder="Name"
                       name="name" value={name}
@@ -45,7 +74,7 @@ const Register = () => {
                  <input type="submit" className="btn btn-primary" value="Register" />
               </form>
             <p className="my-1">
-             Already have an account? <a href="login.html">Sign In</a>
+             Already have an account? <Link to='/login'>Sign In</Link>
             </p>
          </Fragment>
     )
