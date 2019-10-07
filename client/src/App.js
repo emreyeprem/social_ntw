398e64040983fa5d849f36f1bc0c3e3762ade5ea
Emre Akurek <emreyeprem@gmail.com>
2019-10-07 15:08:28 -0500

    post & comment components created

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,17 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Routes from './components/routing/Routes';
 
-import './App.css';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-import setAuthToken from "./utils/setAuthToken";
+import setAuthToken from './utils/setAuthToken';
 
-if(localStorage.token) {
+import './App.css';
+
+if (localStorage.token) {
     setAuthToken(localStorage.token);
 }
 
@@ -23,24 +20,20 @@ const App = () => {
     useEffect(() => {
         store.dispatch(loadUser());
     }, []);
-    return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-           <Route exact path='/' component={Landing} />
-             <section className="container" >
-               <Alert />
-                 <Switch>
-                   <Route exact path='/register' component={Register} />
-                   <Route exact path='/login' component={Login} />
-                   <PrivateRoute exact path='/dashboard' component={Dashboard} />
-                 </Switch>
-             </section>
-         </Fragment>
-      </Router>
-   </Provider>
 
-)};
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                     <Switch>
+                        <Route exact path='/' component={Landing} />
+                        <Route component={Routes} />
+                     </Switch>
+                </Fragment>
+            </Router>
+        </Provider>
+);
+};
 
 export default App;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,7 +6,7 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
-    // ACCOUNT_DELETED
+    ACCOUNT_DELETED
 } from '../actions/types';
 
 const initialState = {
@@ -17,7 +17,7 @@ const initialState = {
 };
 
 export default function(state = initialState, action) {
-    const {type, payload} = action;
+    const { type, payload } = action;
 
     switch (type) {
         case USER_LOADED:
@@ -26,27 +26,28 @@ export default function(state = initialState, action) {
                 isAuthenticated: true,
                 loading: false,
                 user: payload
-            }
+            };
         case REGISTER_SUCCESS:
-        case LOGIN_SUCCESS:     // LOGIN_SUCCESS does the same thing like REGISTER_SUCCESS does therefore we write it under it.
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
                 ...payload,
                 isAuthenticated: true,
                 loading: false
-            }
+            };
         case REGISTER_FAIL:
         case AUTH_ERROR:
         case LOGIN_FAIL:
         case LOGOUT:
+        case ACCOUNT_DELETED:
             localStorage.removeItem('token');
             return {
                 ...state,
                 token: null,
                 isAuthenticated: false,
                 loading: false
-            }
+            };
         default:
             return state;
     }
