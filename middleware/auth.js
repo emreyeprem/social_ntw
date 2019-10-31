e6487b1c2231552d5fff7ae9f7092ee8ca24801b
Emre Akurek <43308547+emreyeprem@users.noreply.github.com>
2019-10-31 16:08:57 -0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,11 +11,11 @@ module.exports = function(req, res, next) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
 
-    // Verify token
+    // if there is token, then Verify token by decoding it
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-        req.user = decoded.user;
+        req.user = decoded.user;   // .user (remember we attached user, user.id in the payload inside users.js)
         next();
     } catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
