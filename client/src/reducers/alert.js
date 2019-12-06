ac099ea42ac41af594affdf38c6e9c21399a4318
Emre Akurek <43308547+emreyeprem@users.noreply.github.com>
2019-12-06 14:44:50 -0600

    Update alert.js

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -9,7 +9,7 @@ export default function(state = initialState, action) {
         case SET_ALERT:
             return [...state, payload];
         case REMOVE_ALERT:
-            return state.filter(alert => alert.id !== payload);
+            return state.filter(alert => alert.id !== payload);    // Payload is gonna be just an ID in this case.
         default:
             return state;
     }
