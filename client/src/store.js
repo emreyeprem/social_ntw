de15f85ece7ee32e2740d741e169b7eff8be70e6
Emre Akurek <43308547+emreyeprem@users.noreply.github.com>
2020-01-09 14:37:18 -0600

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import rootReducer from './reducers';   // root reducer is 'index.js' file in reducers folder.
 
 const initialState = {};
 
