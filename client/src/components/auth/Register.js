c855b2100e6c50ca15c4c64687a2ec1850fb95c9
Emre Akurek <43308547+emreyeprem@users.noreply.github.com>
2020-03-28 16:27:16 -0500

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -23,7 +23,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
         if(password !== password2) {
             setAlert('Password do not match', 'danger');
         }else {
-            register({ name, email, password });
+            register({ name, email, password });  // //register({ name: name, email: email, password: password });
             // we wanted to make the code below happen in redux action not in component, therefore  it's commented out
             // console.log('Success')
             // const newUser = {
